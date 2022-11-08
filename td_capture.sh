dafd91279d4665fd2ee3878f244e59200b0feb38
Samir Caica <samir.caica@gmail.com>
2022-11-07 23:08:59 -0500

    Increased the TD files and the sleep time

--- td_capture.sh
+++ td_capture.sh
@@ -5,10 +5,10 @@ COMPRESSED_FILE="thread_dump_${PID_THREAD}"
 FILE_LIST="jstack-$(hostname)-${PID_THREAD}-*.txt"
 
 echo "Starting capture of jstack for PID ${PID_THREAD}"
-for i in {1..2}; do 
+for i in {1..10}; do 
 	jstack -l ${PID_THREAD}  > jstack-$(hostname)-${PID_THREAD}-$(date "+%Y-%m-%d-%H-%M-%S-%Z").txt;
 	echo "jstack-$(hostname)-${PID_THREAD}-$(date "+%Y-%m-%d-%H-%M-%S-%Z").txt written";
-	sleep 2;
+	sleep 10;
 done
 
 mkdir ${COMPRESSED_FILE}
