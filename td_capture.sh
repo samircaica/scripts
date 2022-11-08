15b57a0570dca4006a82fc25752925f7c8aa1026
Samir Caica <samir.caica@gmail.com>
2022-11-07 23:08:05 -0500

    Modified to create a dir and then remove it

--- td_capture.sh
+++ td_capture.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 PID_THREAD=$1
+COMPRESSED_FILE="thread_dump_${PID_THREAD}"
+FILE_LIST="jstack-$(hostname)-${PID_THREAD}-*.txt"
+
 echo "Starting capture of jstack for PID ${PID_THREAD}"
 for i in {1..2}; do 
-	jstack -l ${PID_THREAD}  > /tmp/jstack-$(hostname)-${PID_THREAD}-$(date "+%Y-%m-%d-%H-%M-%S-%Z").txt;
-	echo "/tmp/jstack-$(hostname)-${PID_THREAD}-$(date "+%Y-%m-%d-%H-%M-%S-%Z").txt written";
+	jstack -l ${PID_THREAD}  > jstack-$(hostname)-${PID_THREAD}-$(date "+%Y-%m-%d-%H-%M-%S-%Z").txt;
+	echo "jstack-$(hostname)-${PID_THREAD}-$(date "+%Y-%m-%d-%H-%M-%S-%Z").txt written";
 	sleep 2;
 done
 
-tar -cvzf thread_dump_${PID_THREAD}.tar.gz /tmp/jstack-$(hostname)-${PID_THREAD}-*.txt
+mkdir ${COMPRESSED_FILE}
+mv ${FILE_LIST} ${COMPRESSED_FILE}
+tar -cvzf ${COMPRESSED_FILE}.tar.gz ${COMPRESSED_FILE} --remove-files
